f1504fd7e1e899a791fb8f65647250b9dd4cd76b
Ariunbold <ariun1118@gmail.com>
2019-07-25 21:20:59 -0400

    Added Sign up form but not functional yet

--- bucketlist-react/src/Sign-up/SignUpAction.js
+++ bucketlist-react/src/Sign-up/SignUpAction.js
@@ -1,1 +1,21 @@
 import axios from 'axios';
+
+
+export const SIGNINGUP_START = 'SIGNINGUP_START';
+export const SIGNINGUP_SUCCESS = 'SIGNINGUP_SUCCESS';
+export const SIGNINGUP_FAILED = 'SIGNINGUP_FAILED';
+
+export function signingUp (){
+    return dispatch => {
+        dispatch({type : SIGNINGUP_START})
+
+        return axios
+         .post (' https://bucket-list-webpt6.herokuapp.com/api/register')
+         .then(res => {
+             dispatch({type: SIGNINGUP_SUCCESS, payload: res.data})
+         })
+         .catch(err => {
+             console.log(err)
+         })   
+    }
+}
--- bucketlist-react/src/Sign-up/signupReducer.js
+++ bucketlist-react/src/Sign-up/signupReducer.js
@@ -1,16 +1,39 @@
-import {} from './SignUpAction'
+import {
+    SIGNINGUP_START,
+    SIGNINGUP_SUCCESS,
+    SIGNINGUP_FAILED
+} from './SignUpAction'
 
 const initialState = {
     todo:[],
+    signingUp: false,
+    signingIn: false,
     fetchingList:false,
     error:null
 }
 
 export const signupReducer = (state = initialState, action) => {
     switch ( action.type){
+        case SIGNINGUP_START:
+            return{
+                ...state,
+                signingUp: true,
+                error: null
+            }
+        case SIGNINGUP_SUCCESS:
+            return{
+                    ...state,
+                    signingUp: true,
+                    error: null
+                }
+        case SIGNINGUP_FAILED:
+                return{
+                    ...state,
+                    signingUp: false,
+                    error: action.payload
+                }
         default:
             return state;
     }
 }
 
-// export default signupReducer;
